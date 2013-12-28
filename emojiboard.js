56256357f51325f8c2d921cf7b5181f393856414
Josh Asch <josh@josh-asch.net>
2013-12-28 13:51:18 +0000

    SNO MANY SWIPES

--- emojiboard.js
+++ emojiboard.js
@@ -1,1 +1,9 @@
-window.mySwipe = Swipe(document.getElementById('symbols'));
+window.peopleSwipe = Swipe(document.getElementById('people-section'));
+
+window.natureSwipe = Swipe(document.getElementById('nature-section'));
+
+window.objectsSwipe = Swipe(document.getElementById('objects-section'));
+
+window.placesSwipe = Swipe(document.getElementById('places-section'));
+
+window.symbolsSwipe = Swipe(document.getElementById('symbols-section'));
